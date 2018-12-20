3cfdef57b1527c5982c5aedbb6f8448ec4540694
Soon <serdeemail@gmail.com>
2018-12-20 17:36:51 +0800

    新增代码注释

--- public/test/rsa/decrypt.js
+++ public/test/rsa/decrypt.js
@@ -20,6 +20,8 @@ router.post('/decrypt', function (req, res, next) {
         // 创建私钥对象
         var privateKey = new NodeRSA(data.toString());
         privateKey.setOptions({
+            // 这里需要指定RSA padding模式为pkcs1，这是因为前端jsencrypt库采用了pkcs1，而后端node-rsa默认使用的pkcs1_oaep
+            // https://stackoverflow.com/questions/33837617/node-rsa-errors-when-trying-to-decrypt-message-with-private-key
             encryptionScheme: 'pkcs1'
         });
 
